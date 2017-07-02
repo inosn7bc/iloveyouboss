ee471df709ec54d285841a9e70e9e9c9565e8b7a
sn1 <sn1@localhost>
2017-07-02 19:14:45 +0900

    7まで終わり

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -16,6 +16,9 @@ public class ProfileTest {
 	public void test() {
 		Profile profile = new Profile("Bull Hockey, Inc.");
 		Question question = new BooleanQuestion(1, "ボーナスは支給されますかx?");
+		
+		Answer profileAnswer = new Answer(question, Bool.FALSE);
+		profile.add(profileAnswer);
 
 		Criteria criteria = new Criteria();
 		Answer criteriaAnswer = new Answer(question, Bool.TRUE);
