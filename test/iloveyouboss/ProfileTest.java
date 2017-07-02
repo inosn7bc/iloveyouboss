6b46f2da38cd498e5a5338723622422ec44771ff
sn1 <sn1@localhost>
2017-07-02 19:13:30 +0900

    インデントを整えた

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -12,14 +12,14 @@ import org.junit.*;
 
 public class ProfileTest {
 
-   @Test
-   public void test() {
-      Profile profile = new Profile("Bull Hockey, Inc.");
-      Question question = new BooleanQuestion(1, "ボーナスは支給されますかx?");
-      
-      Criteria criteria = new Criteria();
-      Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-      Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-      criteria.add(criterion);
-   }
+	@Test
+	public void test() {
+		Profile profile = new Profile("Bull Hockey, Inc.");
+		Question question = new BooleanQuestion(1, "ボーナスは支給されますかx?");
+
+		Criteria criteria = new Criteria();
+		Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+		Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+		criteria.add(criterion);
+	}
 }
